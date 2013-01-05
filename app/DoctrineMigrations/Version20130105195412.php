b946dc1f35d9a7e3f2eebedcbbc26b29b2bbf85a
Barbus Sergiu <danvbe@gmail.com>
2013-01-05 22:13:48 +0200

    doctrine migrations for created/updated entity fields

---
+++ app/DoctrineMigrations/Version20130105195412.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130105195412 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE lcl_user ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL");
+        $this->addSql("ALTER TABLE lcl_log ADD created_at DATETIME NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE lcl_log DROP created_at");
+        $this->addSql("ALTER TABLE lcl_user DROP created_at, DROP updated_at");
+    }
+}
