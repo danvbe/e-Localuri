d26df3fa1517aae941d07566d157402f190e4533
Barbus Sergiu <danvbe@gmail.com>
2013-01-19 22:51:40 +0200

    move templateing of FOSUser to app/resources

--- src/Localuri/UserBundle/LocaluriUserBundle.php
+++ src/Localuri/UserBundle/LocaluriUserBundle.php
@@ -6,12 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LocaluriUserBundle extends Bundle
 {
-    /*
-     * We overwrite this function to tell Symfony thos bundle is child of FosUserBundle,
-     * in order to use the templates from this Bundle's views
-     */
-    public function getParent()
-    {
-        return 'FOSUserBundle';
-    }
 }
